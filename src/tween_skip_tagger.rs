63b6035f2f158f97a14c5230dc53b01fb738a44b
Rabbival <87331993+Rabbival@users.noreply.github.com>
2026-03-02 10:39:37 +0200

    update to match tween alt revert

--- src/tween_skip_tagger.rs
+++ src/tween_skip_tagger.rs
@@ -26,11 +26,9 @@ pub struct TweenSkipTaggerPlugin;
 
 impl Plugin for TweenSkipTaggerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(TweenEventPlugin::<TweenSkipTagTweenRequest>::in_schedule(
-            PostUpdate.intern(),
-        ))
-        .add_observer(listen_to_regular_event_trigger)
-        .add_observer(listen_to_triggers_from_tweens);
+        app.add_plugins(TweenEventPlugin::<TweenSkipTagTweenRequest>::default())
+            .add_observer(listen_to_regular_event_trigger)
+            .add_observer(listen_to_triggers_from_tweens);
     }
 }
 
